eb57a6debdc56233157ad2fbb33ddfbf5399593d
ksandermann <kevin.sandermann@gmail.com>
2020-12-07 10:55:30 +0100

    updated multistage-builder to alpine 3.12.1 and release 2020-12-07

--- multistage-builder/build.sh
+++ multistage-builder/build.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-LPINE_VERSION="3.12.0"
-IMAGE_TAG="2020-06-19"
+LPINE_VERSION="3.12.1"
+IMAGE_TAG="2020-12-07"
 
 docker build \
     --build-arg ALPINE_VERSION=$LPINE_VERSION \
