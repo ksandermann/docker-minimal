742bfdf23ac386a3240cb49356e3103fd12b2ad7
Kevin Sandermann <Kevin.sandermann@gmail.com>
2022-03-17 18:31:27 +0100

    multistage-builder 2022-03-17_01 (#7)

--- multistage-builder/build.sh
+++ multistage-builder/build.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-LPINE_VERSION="3.12.1"
-IMAGE_TAG="2020-12-07"
+LPINE_VERSION="3.14.4"
+IMAGE_TAG="2022-03-17"
 
 docker build \
     --build-arg ALPINE_VERSION=$LPINE_VERSION \
