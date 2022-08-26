65bfe37528b2e263ff2f2a56eec119a1f6ced554
Kevin Sandermann <kevin.sandermann@gmail.com>
2022-08-26 08:55:59 +0200

    deprecated oc-cli, built ansible

--- openshift-cli/build.sh
+++ openshift-cli/build.sh
@@ -1,23 +1,18 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-
-#bash > 4.0 required
-#https://stackoverflow.com/questions/10574969/how-do-i-install-bash-3-2-25-on-mac-os-x-10-5-8
 
 BLDER_VERSION="2022-08-25"
 
 #starting 2018-01-01
-declare -A releaseversions=(
-"4.10.23"
+declare -a releaseversions=(
 "4.11.0"
-
 )
 
 for releaseversion in "${releaseversions[@]}";
 do
     OC_VERSION=$releaseversion
-    docker build \
+    docker buildx build \
         --pull \
         --build-arg BUILDER_VERSION=$BLDER_VERSION \
         --build-arg OC_CLI_VERSION=$OC_VERSION \
